fc7800400124ccce4266b19fa1c99867cfe155d0
ExpertSpace <41064836+ExpertSpace@users.noreply.github.com>
2020-08-04 18:00:57 +0400

    Changed file

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -11,30 +14,65 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
+    TextView text;
+    String txt = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void MyClick(View view) {
-        TextView text = (TextView) findViewById(R.id.textView);
-        EditText editText = (EditText) findViewById(R.id.editTextNumber);
+    
+    public void Copy(View view)
+    {
+        if(text != null)
+        {
+            txt = text.getText().toString();
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("", txt);
+            clipboard.setPrimaryClip(clip);
+        }
+        else
+        {
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("", " ");
+            clipboard.setPrimaryClip(clip);
+        }
+    }
 
-        String txt = editText.getText().toString();
+    public void Generated(View view)
+    {
+        //TextView text = (TextView) findViewById(R.id.textView);
+        text = (TextView) findViewById(R.id.textView);
+        //EditText editText = (EditText) findViewById(R.id.editTextNumber);
+        editText = (EditText) findViewById(R.id.editTextNumber);
+
+        //String txt = editText.getText().toString();
+        txt = editText.getText().toString();
 
         char[] ch = txt.toCharArray();
-        for (int i = 0; i < ch.length; i++) {
-            if (ch[i] < 48 || ch[i] > 57) {
+        for (int i = 0; i < ch.length; i++)
+        {
+            if (ch[i] < 48 || ch[i] > 57)
+            {
                 text.setText("Длина пароля должна состоять только из цифр!");
                 break;
             }
         }
         //else
         {
-            if (txt.isEmpty()) {
-                text.setText("Длина пароля не должна быть нулевой!");
-            } else {
+            if (txt.isEmpty())
+            {
+                text.setText("Поле ввода длины пароля пустое!");
+            }
+            else if(txt.equals("0"))
+            {
+                text.setText("Длина пароля не может быть нулевой!");
+            }
+            else
+            {
                 int length = Integer.parseInt(txt);
 
                 String password = "";
@@ -58,125 +96,150 @@ public class MainActivity extends AppCompatActivity {
                 Switch switchView2 = (Switch) findViewById(R.id.switch2);
                 Switch switchView3 = (Switch) findViewById(R.id.switch3);
 
-                if (switchView0.isChecked() && switchView1.isChecked() && switchView2.isChecked() && switchView3.isChecked()) {
+                if (switchView0.isChecked() && switchView1.isChecked() && switchView2.isChecked() && switchView3.isChecked())
+                {
                     Random random = new Random();
                     //int length = Integer.parseInt(txt);
-                    for (int i = 0; i < length; i++) {
+                    for (int i = 0; i < length; i++)
+                    {
                         int x = random.nextInt(69);
                         password += str[x];
                     }
                     text.setText(password);
                 }
 
-                if (switchView0.isChecked() && !switchView1.isChecked() && !switchView2.isChecked() && !switchView3.isChecked()) {
+                if (switchView0.isChecked() && !switchView1.isChecked() && !switchView2.isChecked() && !switchView3.isChecked())
+                {
                     Random random = new Random();
                     //int length = Integer.parseInt(txt);
-                    for (int i = 0; i < length; i++) {
+                    for (int i = 0; i < length; i++)
+                    {
                         int x = random.nextInt(10);
                         password += digitals[x];
                     }
                     text.setText(password);
                 }
 
-                if (!switchView0.isChecked() && switchView1.isChecked() && !switchView2.isChecked() && !switchView3.isChecked()) {
+                if (!switchView0.isChecked() && switchView1.isChecked() && !switchView2.isChecked() && !switchView3.isChecked())
+                {
                     Random random = new Random();
                     //int length = Integer.parseInt(txt);
-                    for (int i = 0; i < length; i++) {
+                    for (int i = 0; i < length; i++)
+                    {
                         int x = random.nextInt(26);
                         password += letters[x];
                     }
                     text.setText(password);
                 }
 
-                if (!switchView0.isChecked() && switchView1.isChecked() && switchView2.isChecked() && !switchView3.isChecked()) {
+                if (!switchView0.isChecked() && switchView1.isChecked() && switchView2.isChecked() && !switchView3.isChecked())
+                {
                     Random random = new Random();
                     //int length = Integer.parseInt(txt);
-                    for (int i = 0; i < length; i++) {
+                    for (int i = 0; i < length; i++)
+                    {
                         int x = random.nextInt(52);
                         password += different_register[x];
                     }
                     text.setText(password);
                 }
 
-                if (!switchView0.isChecked() && !switchView1.isChecked() && !switchView2.isChecked() && switchView3.isChecked()) {
+                if (!switchView0.isChecked() && !switchView1.isChecked() && !switchView2.isChecked() && switchView3.isChecked())
+                {
                     Random random = new Random();
                     //int length = Integer.parseInt(txt);
-                    for (int i = 0; i < length; i++) {
+                    for (int i = 0; i < length; i++)
+                    {
                         int x = random.nextInt(7);
                         password += symbols[x];
                     }
                     text.setText(password);
                 }
 
-                if (switchView0.isChecked() && switchView1.isChecked() && !switchView2.isChecked() && !switchView3.isChecked()) {
+                if (switchView0.isChecked() && switchView1.isChecked() && !switchView2.isChecked() && !switchView3.isChecked())
+                {
                     Random random = new Random();
                     //int length = Integer.parseInt(txt);
-                    for (int i = 0; i < length; i++) {
+                    for (int i = 0; i < length; i++)
+                    {
                         int x = random.nextInt(36);
                         password += digital_letters[x];
                     }
                     text.setText(password);
                 }
 
-                if (switchView0.isChecked() && switchView1.isChecked() && switchView2.isChecked() && !switchView3.isChecked()) {
+                if (switchView0.isChecked() && switchView1.isChecked() && switchView2.isChecked() && !switchView3.isChecked())
+                {
                     Random random = new Random();
                     //int length = Integer.parseInt(txt);
-                    for (int i = 0; i < length; i++) {
+                    for (int i = 0; i < length; i++)
+                    {
                         int x = random.nextInt(62);
                         password += digital_letters_diff_reg[x];
                     }
                     text.setText(password);
                 }
 
-                if (switchView0.isChecked() && !switchView1.isChecked() && !switchView2.isChecked() && switchView3.isChecked()) {
+                if (switchView0.isChecked() && !switchView1.isChecked() && !switchView2.isChecked() && switchView3.isChecked())
+                {
                     Random random = new Random();
                     //int length = Integer.parseInt(txt);
-                    for (int i = 0; i < length; i++) {
+                    for (int i = 0; i < length; i++)
+                    {
                         int x = random.nextInt(17);
                         password += digital_symbols[x];
                     }
                     text.setText(password);
                 }
 
-                if (!switchView0.isChecked() && switchView1.isChecked() && !switchView2.isChecked() && switchView3.isChecked()) {
+                if (!switchView0.isChecked() && switchView1.isChecked() && !switchView2.isChecked() && switchView3.isChecked())
+                {
                     Random random = new Random();
                     //int length = Integer.parseInt(txt);
-                    for (int i = 0; i < length; i++) {
+                    for (int i = 0; i < length; i++)
+                    {
                         int x = random.nextInt(33);
                         password += letters_symbols[x];
                     }
                     text.setText(password);
                 }
 
-                if (!switchView0.isChecked() && switchView1.isChecked() && switchView2.isChecked() && switchView3.isChecked()) {
+                if (!switchView0.isChecked() && switchView1.isChecked() && switchView2.isChecked() && switchView3.isChecked())
+                {
                     Random random = new Random();
                     //int length = Integer.parseInt(txt);
-                    for (int i = 0; i < length; i++) {
+                    for (int i = 0; i < length; i++)
+                    {
                         int x = random.nextInt(59);
                         password += letters_diff_reg_symbols[x];
                     }
                     text.setText(password);
                 }
 
-                if (switchView0.isChecked() && switchView1.isChecked() && !switchView2.isChecked() && switchView3.isChecked()) {
+                if (switchView0.isChecked() && switchView1.isChecked() && !switchView2.isChecked() && switchView3.isChecked())
+                {
                     Random random = new Random();
                     //int length = Integer.parseInt(txt);
-                    for (int i = 0; i < length; i++) {
+                    for (int i = 0; i < length; i++)
+                    {
                         int x = random.nextInt(42);
                         password += digital_letters_symbol[x];
                     }
                     text.setText(password);
                 }
 
-                if (switchView0.isChecked() && !switchView1.isChecked() && switchView2.isChecked() && switchView3.isChecked()) {
+                if (switchView0.isChecked() && !switchView1.isChecked() && switchView2.isChecked() && switchView3.isChecked())
+                {
                     text.setText("Внимание! Уберите галочку с кнопки ''Разный регистр'', поскольку цифры не имеют регистра.");
                 }
 
-                if (switchView0.isChecked() && !switchView1.isChecked() && switchView2.isChecked() && !switchView3.isChecked()) {
+                if (switchView0.isChecked() && !switchView1.isChecked() && switchView2.isChecked() && !switchView3.isChecked())
+                {
                     text.setText("Внимание! Уберите галочку с кнопки ''Разный регистр'', поскольку цифры не имеют регистра.");
                 }
 
-                if (!switchView0.isChecked() && !switchView1.isChecked() && switchView2.isChecked() && switchView3.isChecked()) {
+                if (!switchView0.isChecked() && !switchView1.isChecked() && switchView2.isChecked() && switchView3.isChecked())
+                {
                     text.setText("Внимание! Уберите галочку с кнопки ''Разный регистр'', поскольку специальные символы не имеют регистра.\");");
                 }
             }
